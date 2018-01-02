dccabbf33860b286ef1f410ea565d336dbd71a86
Naggi <nagase2@gmail.com>
2018-01-02 18:01:08 +0900

    adjustment for timer

--- Nagaden/FirstViewController.swift
+++ Nagaden/FirstViewController.swift
@@ -11,10 +11,19 @@ import UIKit
 class FirstViewController: UIViewController {
 
     
+    @IBOutlet weak var firstWebView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         NSLog("first viewがロードされました")
+  
+        let url = URL(string: "https://m2x.att.com/catalog/df834a7986e9a52d5d28e46dd97e87df")
+
+        firstWebView.loadRequest(URLRequest(url: url!))
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        NSLog("first viewが表示されました")
+       // firstWebView.reload()
     }
 
     override func didReceiveMemoryWarning() {
--- Nagaden/SecondViewController.swift
+++ Nagaden/SecondViewController.swift
@@ -10,15 +10,46 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    
+    @IBOutlet weak var secoundWebView: UIWebView!
+    var timer: Timer!
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        timer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
+        timer.fire()
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        //１timer.
+        NSLog("別の画面に移りました")
+        //TODO:ここで一旦タイマーを停止する必要がある。
+        timer.invalidate()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         NSLog("second viewがロードされました。")
+        //let url = URL(string: "https://m2x.att.com/catalog/df834a7986e9a52d5d28e46dd97e87df")
+        let url = URL(string: "https://api-m2x.att.com/v2/charts/d18699a29eedf08446d254ea187bf796.svg?width=1000&height=750&type=avg&interval=500")
+        secoundWebView.loadRequest(URLRequest(url: url!))
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        NSLog("second viewが表示されました")
+        secoundWebView.reload()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func update(tm: Timer) {
+        // do something
+        NSLog("タイマーが呼ばれました")
+        secoundWebView.reload()
     }
 
 
