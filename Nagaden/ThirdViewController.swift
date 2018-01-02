6dab169e3062656ff94ccdb9d4d12001130f54ab
Naggi <nagase2@gmail.com>
2018-01-03 02:04:37 +0900

    大体できた

--- Nagaden/ThirdViewController.swift
+++ Nagaden/ThirdViewController.swift
@@ -15,6 +15,8 @@ class ThirdViewController: UIViewController {
     @IBOutlet weak var thirdWebView: UIWebView!
     
   
+    @IBOutlet weak var fromDate: UIDatePicker!
+    @IBOutlet weak var toDate: UIDatePicker!
     
     @IBAction func printButton(_ sender: Any) {
         NSLog("ボタンが押されました")
@@ -49,15 +51,54 @@ class ThirdViewController: UIViewController {
                 }
             }
         }
-        
-        
-        
+    }
+    @IBAction func fromDateChanged(_ sender: UIDatePicker) {
+        NSLog("From日付が変更されました")
+        reloadGraph()
+    }
+    @IBAction func toDateChanged(_ sender: UIDatePicker) {
+        NSLog("To日付が変更されました")
+        reloadGraph()
     }
     
     
+    func reloadGraph(){
+        //FromとToの日付を取得
+        //Make URL 
+        //do reload
+        
+        let calendar = Calendar.current
+        
+        let fromYear = String(format:"%04d",calendar.component(.year,from:fromDate.date))
+        let fromMonth = String(format:"%02d",calendar.component(.month,from:fromDate.date))
+        let fromDay = String(format:"%02d",calendar.component(.day,from:fromDate.date))
+        
+        let toYear = String(format:"%04d",calendar.component(.year,from:toDate.date))
+        let toMonth = String(format:"%02d",calendar.component(.month,from:toDate.date))
+        let toDay = String(format:"%02d",calendar.component(.day,from:toDate.date))
+        
+        let f = DateFormatter()
+        f.timeStyle = .none
+        f.dateStyle = .long
+        f.locale = Locale(identifier: "ja_JP")
+        
+            
+        NSLog(f.string(from: fromDate.date))
+        NSLog(fromYear)
+        NSLog(fromMonth)
+        NSLog(fromDay)
+        
+       // NSLog(fromDate.date.)
+        url = URL(string: "https://api-m2x.att.com/v2/charts/d18699a29eedf08446d254ea187bf796.svg?width=1000&height=700&type=avg&interval=6000&start="+fromYear+"-"+fromMonth+"-"+fromDay+"T15%3A00%3A00.000Z&end="+toYear+"-"+toMonth+"-"+toDay+"T15%3A00%3A00.000Z")
+        
+        thirdWebView.loadRequest(URLRequest(url: url!))
+    }
+    
+    var url = URL(string: "https://api-m2x.att.com/v2/charts/d18699a29eedf08446d254ea187bf796.svg?width=1000&height=700&type=avg&interval=6000&start=2018-01-01T15%3A00%3A00.000Z&end=2018-01-30T15%3A00%3A00.000Z")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = URL(string: "https://api-m2x.att.com/v2/charts/d18699a29eedf08446d254ea187bf796.svg?width=800&height=600&type=avg&interval=6000&start=2018-01-01T15%3A00%3A00.000Z&end=2018-01-30T15%3A00%3A00.000Z")
+        
         thirdWebView.loadRequest(URLRequest(url: url!))
     
     }
@@ -67,10 +108,6 @@ class ThirdViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
         
-        
     }
-
-    
-    
     
 }
